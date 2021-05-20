cd5b55721ae5f4ccd535d835ab079e64aa3f5030
renan-auer <renan.auer@gmail.com>
2021-05-20 02:09:21 -0300

    feat: get current code

--- src/main/java/com/paripassu/teste/services/ObterSenhaAtualService.java
+++ src/main/java/com/paripassu/teste/services/ObterSenhaAtualService.java
@@ -1,9 +1,12 @@
 package com.paripassu.teste.services;
 
+import com.paripassu.teste.enums.TipoSenha;
 import com.paripassu.teste.models.Senha;
 import com.paripassu.teste.repository.SenhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ObterSenhaAtualService {
@@ -13,6 +16,11 @@ public class ObterSenhaAtualService {
 
     public Senha get() {
         Senha ultimaSenha = this.senhaRepository.getSenhaAtual();
+
+        if(ultimaSenha == null) {
+            ultimaSenha = new Senha(0, "0000", TipoSenha.NORMAL, new Date());
+        }
+
         return ultimaSenha;
     }
 }
