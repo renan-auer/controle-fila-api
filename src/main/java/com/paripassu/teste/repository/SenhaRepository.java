bd9b519d4b0b224429a54bd27e86301aa3cf9a82
renan-auer <renan.auer@gmail.com>
2021-05-20 02:14:47 -0300

    feat: senha repository

--- src/main/java/com/paripassu/teste/repository/SenhaRepository.java
+++ src/main/java/com/paripassu/teste/repository/SenhaRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface SenhaRepository extends JpaRepository<Senha, Integer> {
 
-    @Query(value = "select * from senha where ja_chamada = false order by  tipo desc, sequencia asc LIMIT 1", nativeQuery = true)
+    @Query(value =  "select * from senha " +
+                    "where atual = true or ja_chamada = false " +
+                    "order by atual desc, tipo desc, sequencia asc LIMIT 1", nativeQuery = true)
     Senha getSenhaAtual();
 
     @Query(value = "SELECT * FROM senha WHERE tipo = ?1 and ja_chamada = false ORDER BY id DESC  LIMIT 1", nativeQuery = true)
